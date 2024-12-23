febd713285ca5c037a72a6cd7799157f7c229db1
= <harry940816@gmail.com>
2024-12-23 16:14:20 +0800

    organize APIs and add mainpage

--- SQL/load_data.sql
+++ SQL/load_data.sql
@@ -1,5 +1,5 @@
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/player_name_id_map.csv' 
-INTO TABLE name_id_map
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/nba_teams_with_correct_team_ids.csv' 
+INTO TABLE nba_teams
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
