04c303656c9c53e14c7bc8aab7cb45678231ca5d
SblackOuO <rain6111213@gmail.com>
2024-12-24 15:49:03 +0800

    直接複製一個member_main_page當作登入之後的介面 要再改

--- web_pages/app.py
+++ web_pages/app.py
@@ -89,7 +89,6 @@ def register():
             cursor.close()
             conn.close()
 
-
 # 登入功能
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -110,7 +109,7 @@ def login():
             if user:
                 session['user'] = username  # 設置 session
                 print(f"User {username} logged in successfully.")
-                return redirect('/')
+                return redirect('/main_page')
             else:
                 return render_template('login.html', login_error=True)
         except Exception as e:
@@ -126,13 +125,19 @@ def logout():
     session.pop('user', None)  # 清理 'user' 鍵
     return redirect('/login')
 
-
-
-# main page
+# main page guest
 @app.route('/')
 def main_page():
     return render_template('main_page.html')
 
+# main page member
+@app.route('/main_page')
+def member_main_page():
+    return render_template('member_main_page.html')
+
+@app.route('/teams')
+def teams():
+    return render_template('')
 
 #------------------------login------------------------------------
 ##################################################################
